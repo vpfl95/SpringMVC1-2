590c04f5b4ef7e4c935783bb7947d8ba870a168c
vpfl95 <k_dh610@naver.com>
2023-03-24 18:21:16 +0900

    Initial commit

---
+++ src/main/java/hello/SpringMVC12/SpringMvc12Application.java
@@ -0,0 +1,13 @@
+package hello.SpringMVC12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc12Application.class, args);
+	}
+
+}
